4a3168c69583b5459509e2342a1ea57e56c68516
Shane Moyo <shanemoyo@macs-MacBook-Pro.local>
2021-05-20 09:19:10 -0700

    Create function to create invitations and create function to handle trasform logic

--- index.js
+++ index.js
@@ -9,13 +9,26 @@ const get = async() => {
     }
 }
 
+// Main transform logic lives here. 
+function transform(partners) { 
+    //TODO: Return correctly trasfromed array of countries
+    return; 
+}
+
+function createInvitations(partners) {
+    if(!partners) return null; 
+    const countries = transform(partners);
+    return { countries };
+}
+
 
 async function app() {
     try {
         const { partners } = await get();
         if(partners) {
-            console.log('partners: ', partners)
-            
+            console.log('partners: ', partners); 
+            const invitations = createInvitations(partners);
+            //TODO: Post created invitations to api  
         }
     } catch(err) {
         console.log('error: ', err);
